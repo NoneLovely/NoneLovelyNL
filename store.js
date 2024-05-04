62b31aba9e7af82e440dc7e25de9b90cf2418651
Ермилов Иван <150592200+NoneLovely@users.noreply.github.com>
2024-05-04 18:23:30 +0500

    Update store.js

--- store.js
+++ store.js
@@ -22,7 +22,7 @@ $.get(url + "?method=getFeedback", function(data, status){
             fb = document.createElement('div')
             
             fb.innerHTML = `
-            <div class="rounded-xl shadow-xl bg-black bg-opacity-5 my-5 mx-5 p-2.5">
+            <div class="rounded-xl shadow-xl bg-black bg-opacity-5 my-5 mx-2.5 p-2.5">
                 <p>
                 `+ result[i][2] +` - `+ result[i][0] +` <i class="fi fi-sr-circle-star"></i>
                 </p>
@@ -47,13 +47,16 @@ $.get(url + "?method=getPartners", function(data, status){
             p = document.createElement('div')
 
             p.classList.add("my-5")
+            p.classList.add("border-y-2")
+            p.classList.add("border-white")
+            p.classList.add("border-dashed")
             
             p.innerHTML = `
             <p class="font-bold tracking-widest my-5">`+ result[i][0] +`</p>
             <details class="text-left [&_svg]:open:-rotate-180">
-                <summary class="flex cursor-pointer list-none items-center gap-4 text-black">
+                <summary class="flex p-2.5 cursor-pointer list-none items-center gap-4">
                     <div>
-                        <svg class="rotate-0 transform text-black transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                        <svg class="rotate-0 transform transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                             <polyline points="6 9 12 15 18 9"></polyline>
                         </svg>
                     </div>
