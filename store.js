bcf606bf866d16c3fcaa8292b37c0ea456ea65f5
Ермилов Иван <150592200+NoneLovely@users.noreply.github.com>
2024-05-04 11:18:21 +0500

    Update store.js

--- store.js
+++ store.js
@@ -1,5 +1,6 @@
 url = "https://script.google.com/macros/s/AKfycbwD8GdCWZ28aWOdC7H0Dw0Y2bkc6crhvi7NpZ0YJnV4BN2hw2DHGPVJema-bafNQmnV/exec"
 
+
 $.get(url + "?method=getOrder", function(data, status){
     result = data["NLS"]
     k = 0
@@ -23,8 +24,9 @@ $.get(url + "?method=getFeedback", function(data, status){
             fb.innerHTML = `
             <div class="rounded-xl shadow-xl bg-black bg-opacity-5 my-5 mx-5 p-2.5">
                 <p>
-                `+ result[i][2] +` - `+ result[i][0] +`/5
+                `+ result[i][2] +` - `+ result[i][0] +` <i class="fi fi-sr-circle-star"></i>
                 </p>
+                <hr class="border-black border-2 my-2.5">
                 <p>
                 `+ result[i][1] +`
                 </p>
@@ -47,9 +49,16 @@ $.get(url + "?method=getPartners", function(data, status){
             p.classList.add("my-5")
             
             p.innerHTML = `
-            <p class="font-bold tracking-widest">`+ result[i][0] +`</p>
-            <details class="text-left">
-                <summary>подробнее</summary>
+            <p class="font-bold tracking-widest my-5">`+ result[i][0] +`</p>
+            <details class="text-left [&_svg]:open:-rotate-180">
+                <summary class="flex cursor-pointer list-none items-center gap-4 text-black">
+                    <div>
+                        <svg class="rotate-0 transform text-black transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                            <polyline points="6 9 12 15 18 9"></polyline>
+                        </svg>
+                    </div>
+                    подробнее
+                </summary>
                 <p>`+ result[i][1] +`</p>
             </details>
             `
